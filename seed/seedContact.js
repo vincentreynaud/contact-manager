2cda394d517b1a473941cb6f8c04ea84821f0c80
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-01-22 15:19:58 +0100

    optimise contact seeder & add general variables

--- seed/seedContact.js
+++ seed/seedContact.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const faker = require("faker");
-faker.locale = "de";
 
 const Contact = require("../models/Contact");
+const { CONTACT_ENTRY_TYPE } = require("../lib/constants");
+faker.locale = "de";
 
 async function seedContact() {
   mongoose.connect(
@@ -11,50 +12,30 @@ async function seedContact() {
   );
   mongoose.connection.on("error", console.error);
 
-  await Contact.remove({});
+  await Contact.deleteMany({});
   console.log("contacts purged!");
 
   const contactPromises = Array(15)
     .fill(null)
     .map(() => {
+      const firstName = faker.name.firstName();
+      const lastName = faker.name.lastName();
+
       const contact = new Contact({
         contact: {
-          first_name: faker.name.firstName(),
-          last_name: faker.name.lastName()
+          first_name: firstName,
+          last_name: lastName
         },
         phones: [
           {
             type: "mobile",
-            country_code: null,
             number: faker.phone.phoneNumberFormat(2)
           }
         ],
         emails: [
           {
-            type: "home",
-            address: faker.internet.email()
-          }
-        ],
-        addresses: [
-          {
-            type: "null",
-            street_one: null,
-            street_two: null,
-            zip: null,
-            city: null,
-            country: null
-          }
-        ],
-        url: [
-          {
-            type: "null",
-            url: null
-          }
-        ],
-        social_profile: [
-          {
-            type: "null",
-            url: null
+            type: faker.random.arrayElement(CONTACT_ENTRY_TYPE),
+            address: faker.internet.email(firstName, lastName)
           }
         ]
       });
