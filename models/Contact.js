8d15ca0a95aead047a3498a83d2d48abbaf00730
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-01-22 15:18:36 +0100

    optimise ContactSchema by creating sub documents & type functions

--- models/Contact.js
+++ models/Contact.js
@@ -1,65 +1,73 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const trimmedString = options => ({
+  type: String,
+  trim: true,
+  ...options
+});
+
+const contactEntryTypeEnum = (types = []) => ({
+  type: String,
+  enum: [...types, "home", "work", "other", "null"],
+  default: "null"
+});
+
+const NameSchema = new Schema(
+  {
+    first_name: trimmedString({ required: true }),
+    last_name: trimmedString()
+  },
+  { _id: false }
+);
+
+const PhoneSchema = new Schema(
+  {
+    type: contactEntryTypeEnum(["mobile", "main"]),
+    country_code: Number,
+    number: trimmedString()
+  },
+  { _id: false }
+);
+
+const EmailSchema = new Schema(
+  {
+    type: contactEntryTypeEnum(),
+    address: trimmedString()
+  },
+  { _id: false }
+);
+
+const AddressSchema = new Schema(
+  {
+    type: contactEntryTypeEnum(),
+    street_one: trimmedString(),
+    street_two: trimmedString(),
+    zip: Number,
+    city: trimmedString(),
+    country: trimmedString()
+  },
+  { _id: false }
+);
+
+const UrlSchema = new Schema(
+  {
+    type: contactEntryTypeEnum(["website"]),
+    url: trimmedString()
+  },
+  { _id: false }
+);
+
 const ContactSchema = new Schema({
   contact: {
-    first_name: {
-      type: String,
-      required: true
-    },
-    last_name: String,
-    company: String
+    type: NameSchema,
+    required: true
   },
-  phones: [
-    {
-      type: {
-        type: String,
-        enum: ["home", "mobile", "work", "main", "other", "null"]
-      },
-      country_code: String,
-      number: String
-    }
-  ],
-  emails: [
-    {
-      type: {
-        type: String,
-        enum: ["home", "work", "other", "null"]
-      },
-      address: String
-    }
-  ],
-  addresses: [
-    {
-      type: {
-        type: String,
-        enum: ["home", "work", "other", "null"]
-      },
-      street_one: String,
-      street_two: String,
-      zip: Number,
-      city: String,
-      country: String
-    }
-  ],
-  url: [
-    {
-      type: {
-        type: String,
-        enum: ["website", "home", "work", "other", "null"]
-      },
-      url: String
-    }
-  ],
-  social_profile: [
-    {
-      type: {
-        type: String,
-        enum: ["facebook", "twitter", "linkedin", "flickr", "other", "null"]
-      },
-      url: String
-    }
-  ]
+  company: trimmedString(),
+  phones: [PhoneSchema],
+  emails: [EmailSchema],
+  addresses: [AddressSchema],
+  url: [UrlSchema]
 });
 
 module.exports = mongoose.model("Contact", ContactSchema);
