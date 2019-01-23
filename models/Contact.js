90403363eea38325bd9047257c52a36393501d06
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-01-23 15:51:22 +0100

    implement trimSchemaString helper to Contact Schema

--- models/Contact.js
+++ models/Contact.js
@@ -1,73 +1,77 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
+const { trimSchemaStrings, contactEntryTypeEnum } = require("../lib/helpers");
+
+const NameSchema = new Schema(
+  trimSchemaStrings({
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: String
+  }),
+  { _id: false }
+);
+
+const PhoneSchema = new Schema(
+  trimSchemaStrings({
+    type: contactEntryTypeEnum(["mobile", "main"]),
+    country_code: Number,
+    number: String
+  }),
+  { _id: false }
+);
+
+const EmailSchema = new Schema(
+  trimSchemaStrings({
+    type: contactEntryTypeEnum(),
+    address: String
+  }),
+  { _id: false }
+);
+
+const AddressSchema = new Schema(
+  trimSchemaStrings({
+    type: contactEntryTypeEnum(),
+    street_one: String,
+    street_two: String,
+    zip: Number,
+    city: String,
+    country: String
+  }),
+  { _id: false }
+);
+
+const UrlSchema = new Schema(
+  trimSchemaStrings({
+    type: contactEntryTypeEnum(["website"]),
+    url: String
+  }),
+  { _id: false }
+);
+
+const ContactSchema = new Schema(
+  trimSchemaStrings({
+    contact: {
+      type: NameSchema,
+      required: true
+    },
+    company: String,
+    phones: [PhoneSchema],
+    emails: [EmailSchema],
+    addresses: [AddressSchema],
+    url: [UrlSchema]
+  })
+);
+
+module.exports = mongoose.model("Contact", ContactSchema);
+
+/*
+// first approach
 const trimmedString = options => ({
   type: String,
   trim: true,
   ...options
 });
-
-const contactEntryTypeEnum = (types = []) => ({
-  type: String,
-  enum: [...types, "home", "work", "other", "null"],
-  default: "null"
-});
-
-const NameSchema = new Schema(
-  {
-    first_name: trimmedString({ required: true }),
-    last_name: trimmedString()
-  },
-  { _id: false }
-);
-
-const PhoneSchema = new Schema(
-  {
-    type: contactEntryTypeEnum(["mobile", "main"]),
-    country_code: Number,
-    number: trimmedString()
-  },
-  { _id: false }
-);
-
-const EmailSchema = new Schema(
-  {
-    type: contactEntryTypeEnum(),
-    address: trimmedString()
-  },
-  { _id: false }
-);
-
-const AddressSchema = new Schema(
-  {
-    type: contactEntryTypeEnum(),
-    street_one: trimmedString(),
-    street_two: trimmedString(),
-    zip: Number,
-    city: trimmedString(),
-    country: trimmedString()
-  },
-  { _id: false }
-);
-
-const UrlSchema = new Schema(
-  {
-    type: contactEntryTypeEnum(["website"]),
-    url: trimmedString()
-  },
-  { _id: false }
-);
-
-const ContactSchema = new Schema({
-  contact: {
-    type: NameSchema,
-    required: true
-  },
-  company: trimmedString(),
-  phones: [PhoneSchema],
-  emails: [EmailSchema],
-  addresses: [AddressSchema],
-  url: [UrlSchema]
-});
-
-module.exports = mongoose.model("Contact", ContactSchema);
+*/
