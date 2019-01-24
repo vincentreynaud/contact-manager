ab1092da12767fc178b22340580344079c17fe9e
Vincent Reynaud <vincentreynaud.de@gmail.com>
2019-01-24 12:09:02 +0100

    add form entries

--- fontend/src/components/Form.jsx
+++ fontend/src/components/Form.jsx
@@ -10,7 +10,7 @@ class Form extends Component {
             type="text"
             className="form-control"
             id="first_name"
-            placeholder="Type First name"
+            placeholder="Type First Name"
             // ref={this.inputFirstName}
           />
         </div>
@@ -20,10 +20,62 @@ class Form extends Component {
             type="text"
             className="form-control"
             id="first_name"
-            placeholder="Type Last name"
+            placeholder="Type Last Name"
             // ref={this.inputFirstName}
           />
         </div>
+        <fieldset>
+          <legend>Phones</legend>
+          <div className="input-group mb-3">
+            <div class="input-group-prepend">
+              <select class="custom-select" id="phone_type" required>
+                <option selected>Type...</option>
+                <option value="mobile">Mobile</option>
+                <option value="home">Home</option>
+                <option value="work">Work</option>
+              </select>
+            </div>
+            <input
+              type="tel"
+              className="form-control"
+              id="phone_country_code"
+              name="phone"
+              pattern="[+][0-9]{2}"
+              placeholder="Country Code"
+              // ref={this.inputFirstName}
+            />
+            <input
+              type="tel"
+              className="form-control"
+              id="phone_number"
+              pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+              placeholder="Phone Number"
+              // ref={this.inputFirstName}
+            />
+          </div>
+        </fieldset>
+        <fieldset>
+          <legend>Emails</legend>
+          <div className="input-group mb-3">
+            <div class="input-group-prepend">
+              <select class="custom-select" id="email_type" required>
+                <option selected>Type...</option>
+                <option value="home">Home</option>
+                <option value="work">Work</option>
+                <option value="other">Other</option>
+              </select>
+            </div>
+            <input
+              type="email"
+              className="form-control"
+              id="email_address"
+              pattern=".+@."
+              size="30"
+              placeholder="example@example.com"
+              // ref={this.inputFirstName}
+            />
+          </div>
+        </fieldset>
       </form>
     );
   }
--- fontend/src/components/Header.jsx
+++ fontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ class Header extends Component {
   render() {
     return (
       <div className="">
-        <h1>Add conctact</h1>
+        <h1>New Contact</h1>
       </div>
     );
   }
